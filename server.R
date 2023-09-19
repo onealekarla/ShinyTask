ebf0f3f64fda30b076d35ad675f34b8497034f16
onealekarla <onealekarla@opsi.co.zagit config --global user.email karla.oneale@opsi.co.za>
2023-09-19 10:08:49 +0200

    Added Comments and some optimization

--- server.R
+++ server.R
@@ -1,43 +1,68 @@
 server <- function(input, output) {
+  
+  #Allow for the upload of larger files
   options(shiny.maxRequestSize=30*1024^2)
   
-  gps_df <<- reactive({
+  #Define a reactive function to read and process the uploaded GPS data
+  gps_df <<- eventReactive(input$gps_file,{
+    #Ensure that file was uploaded
     req(input$gps_file, TRUE, file.exists(input$gps_file$datapath))
+    
+    #Load data based on file type
     if (input$gps_file$type == 'text/csv') {
-      read.csv(input$gps_file$datapath,TRUE, sep=";") %>%
-        mutate(timediff = c(0,diff(timesecs)))
+      data <- read.csv(input$gps_file$datapath,TRUE, sep=";")
     } else {
-      read.xlsx(input$gps_file$datapath, detectDates = FALSE) %>%
-        mutate(GPSDateTime = as.Date(floor(GPSDateTime), origin = '1899-12-30'),
-               timediff = c(0,diff(timesecs))
-        )
-    } 
+      data <- read.xlsx(input$gps_file$datapath, detectDates = FALSE)
+      data$GPSDateTime <- as.Date(floor(data$GPSDateTime), origin = '1899-12-30')
+    }
+    
+    return(data)
   })
   
-  output$vehicles = renderUI({
-    selectizeInput('vehicle',
-                   'Select Vehicle:',
-                   unique(gps_df()$VEHICLE), 
-                   multiple = FALSE,
-                   options = list(
-                     placeholder = "Select Vehicle",
-                     onInitialize = I('function() { this.setValue(""); }')
-                   )
+  plot_points <- eventReactive(input$plot,{
+  
+    #Filter based on user selection of vehicle and date
+    filtered_data <- filter(gps_df(), 
+                            VEHICLE == input$vehicle, 
+                            GPSDateTime == input$date
     )
+    
+    #Add a column 'timediff' to be used in speed calculation
+    filtered_data <- mutate(filtered_data, timediff = c(0,diff(timesecs)))
+    
+    return(filtered_data)
   })
   
+  #Define a dynamic UI element for selecting vehicles based on the data provided
+  output$vehicles = renderUI({
+    if (!(is.null(input$gps_file) || input$gps_file$name == '')) {
+      selectizeInput('vehicle',
+                     'Select Vehicle:',
+                     unique(gps_df()$VEHICLE), 
+                     multiple = FALSE,
+                     options = list(
+                       placeholder = "Select Vehicle",
+                       onInitialize = I('function() { this.setValue(""); }')
+                     )
+      )
+    }
+  })
+  
+  #Define a dynamic dateInput for selecting dates as soon as a vehicle was selected
   output$dates = renderUI({
     if (!is.null(input$vehicle) && input$vehicle != "") {
       dateInput('date','Select Date:', value = '')
     }
   })
   
+  #Define a dynamic button to submit user input
   output$submit = renderUI({
     if (length(input$date)>0) {
       actionButton('plot','Submit')  
     }
   })
   
+  #Define hint / instructions based on user inputs
   output$hint <- renderText({
     if (is.null(input$gps_file) || input$gps_file$name == '') {
       txt <- "Upload a file to continue."
@@ -51,29 +76,18 @@ server <- function(input, output) {
     txt
   })
   
-  plot_points <- eventReactive(input$plot,{
-    filter(gps_df(), 
-           VEHICLE == input$vehicle, 
-           GPSDateTime == input$date
-    )
-  })
-  
-  observe({
-    if (!is.null(input$plot)) {
-      shinyjs::show("body_content")
-    } else {
-      shinyjs::hide("body_content")
-    }
-  })
-  
+  #Calculate total distance when button clicked
   total_distance <- eventReactive(input$plot,{
     sum(plot_points()$Distance)
   })
   
+  #Calculate average speed when button clicked
+  #Time where Distance is zero, excluded from total travel time
   avg_speed <- eventReactive(input$plot,{
     round(total_distance()/sum(plot_points()$timediff[plot_points()$Distance != 0] / 3600))
   })
   
+  #Define output for the speed value box
   output$distance <- renderValueBox(
     valueBox(
       value = paste(
@@ -86,9 +100,11 @@ server <- function(input, output) {
     )
   )
   
+  #Set default color and define output for average speed
   box_color <- 'green'
     output$speed <- renderValueBox({
     
+    #Show warning and change color of value box to red
     if (avg_speed() > 35) {
       box_color = "red"
       shinyalert(
@@ -98,6 +114,7 @@ server <- function(input, output) {
       ) 
     }
     
+    #Define value box
     valueBox(
       value = paste(
         avg_speed(),
@@ -109,25 +126,33 @@ server <- function(input, output) {
     )
   })
   
+  #Define output for data table, excluding empty columns
   output$table <- DT::renderDataTable(options = list(scrollX = TRUE),{
     data <- plot_points() %>%
       select(where(~ any(!is.na(.))))
     data
   })
   
+  #Define output for Leaflet Map
   output$plot <- renderLeaflet({
     leaflet(plot_points) %>%
       addTiles() %>%
+      
+      #Show GPS points based on coordinates
       addCircleMarkers(
         lng = (plot_points()$X),
         lat = (plot_points()$Y),
         radius = 2,
         weight = 2
       ) %>%
+      
+      #Show lines between points
       addPolylines(plot_points()$X,plot_points()$Y,
                    group = 'Routes',
                    color = 'black'
       ) %>%
+      
+      #Allow user to show / hide route lines
       addLayersControl(overlayGroups = c('Routes'))
   })
 }
--- ui.R
+++ ui.R
@@ -1,41 +1,57 @@
 ui <- shinyUI({
   
+  #Create the dashboard header, titled 'Vehicle History'
   header <- dashboardHeader(
     title = "Vehicle History"
   )
   
+  #Create the dashboard sidebar
   sidebar <- dashboardSidebar(
-    useShinyjs(),
     
+    #Change the look of the dashboard
     shinyDashboardThemes("grey_dark"),
     
+    #Add a file input widget for uploading a single *.csv or *.xlsx file
     fileInput('gps_file','Upload Data:', multiple = FALSE, accept = c('.csv', '.xlsx')),
+    
+    #UI elements for vehicle and date selection
+    #Dynamically generated based on whether a user has provided the necessary input
     uiOutput('vehicles'),
     uiOutput('dates'),
+    
+    #Creates a center-aligned 'Submit' button
+    #Dynamically generated based on whether a user has provided the necessary input
     div(
       uiOutput('submit'),
       align = "center"
     ),
+    
+    #Shows text providing hints / instructions
     div(class="textOutput",
       textOutput('hint')
     )
-    
-    
   )
   
+  
+  #Create the main body of the dasboard
   body <- dashboardBody(
     id = "body_content",
-                        
-      tags$head(
-        tags$link(rel = "stylesheet", type = "text/css",  href = "style.css")
-      ),
+      
+      #Create a row with two value boxes displaying the total 
+      #distance and average speed by vehicle and date                  
       fluidRow(
         valueBoxOutput("distance", width = 6),
         valueBoxOutput("speed", width = 6)
       ),
+    
+      #Create a row with a tabbed interface for the map and table views
       fluidRow(
         tabBox(id = "plot_box",
+               
+            #Map View tab
             tabPanel("Map View",leafletOutput("plot")),
+            
+            #Table View tab
             tabPanel("Table View",
                DT::dataTableOutput("table")),
                width = 12
@@ -43,6 +59,7 @@ ui <- shinyUI({
       )
   )
   
+  #Combine the dashboard elements to create the final UI
   ui <- dashboardPage(header, sidebar, body)
   
 })
